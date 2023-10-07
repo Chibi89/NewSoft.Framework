fff23e55e9bb73a4ce106a57ebca54108df1296b
Chibi89 <morganagiuseppe@hotmail.it>
2023-10-07 14:41:33 +0200

    Update Functions.vb
    Add Optional Paramter's

--- Function/Functions.vb
+++ Function/Functions.vb
@@ -1,4 +1,4 @@
-﻿'@2023 Newspoint Software
+'@2023 Newspoint Software
 Imports Microsoft.VisualBasic.Devices
 Imports System.Security.Principal
 Imports Microsoft.Win32
@@ -6,6 +6,7 @@ Imports System.IO
 Imports System.Runtime.InteropServices
 Imports System.Windows.Forms
 Imports System.Drawing
+Imports NewSoft.Framework.PrivateDiagnostic
 ''' <summary>
 ''' indice per poter ottenere varie funzioni che facilitano lo sviluppo nell'ide.
 ''' </summary>
@@ -22,26 +23,30 @@ Public Module PublicFunctions
         ''' </summary>
         Public Shared Sub FormTitle(form As Form, formText As String)
             form.Text = formText
+            TechTacker.TrackFunctionUsage($"FormTitle :: {form} - {formText}")
         End Sub
         ''' <summary>
         ''' indice per poter settare le dimensioni del form.
         ''' FormSize(myForm, 800, 600)
         ''' </summary>
-        Public Shared Sub formSize(form As Form, width As Integer, height As Integer)
+        Public Shared Sub FormSize(form As Form, Optional ByVal width As Integer = Nothing, Optional ByVal height As Integer = Nothing)
             form.Size = New Size(width, height)
+            TechTacker.TrackFunctionUsage($"FormSize :: {form} - {width}x{height}")
         End Sub
         ''' <summary>
         ''' indice per poter settare il trasparancykey color.
         ''' FormTransparencyKey(myForm, Color.White)
         ''' </summary>
-        Public Shared Sub formTransparencyKey(form As Form, transparencyColor As Color)
+        Public Shared Sub FormTransparencyKey(form As Form, transparencyColor As Color)
             form.TransparencyKey = transparencyColor
+            TechTacker.TrackFunctionUsage($"FormTrasparencyColorKey :: {form} - {transparencyColor}")
         End Sub
         ''' <summary>
         ''' indice per poter settare lo start position.
-        ''' FormStartPositions(myForm, "centerscreen")
+        ''' FormStartPositions(myForm, "manual/centerscreen/centerparent")
         ''' </summary>
-        Public Shared Sub formStartPositions(form As Form, position As String)
+        Public Shared Sub FormStartPositions(form As Form, position As String)
+            TechTacker.TrackFunctionUsage($"StartPosition :: {form} - {position}")
             Select Case position.ToLower()
                 Case "manual"
                     form.StartPosition = FormStartPosition.Manual
@@ -55,9 +60,9 @@ Public Module PublicFunctions
         End Sub
         ''' <summary>
         ''' indice per poter settare il maximizebox e il minimizebox del form.
-        ''' FormVisibleTopControl(myForm, True, True)
+        ''' FormVisibleTopControl(myForm, True/False, True/False)
         ''' </summary>
-        Public Shared Sub formVisibleTopControl(form As Form, maximizeBox As Boolean, minimizeBox As Boolean)
+        Public Shared Sub FormVisibleTopControl(form As Form, Optional ByVal maximizeBox As Boolean = Nothing, Optional ByVal minimizeBox As Boolean = Nothing)
             form.MaximizeBox = maximizeBox
             form.MinimizeBox = minimizeBox
         End Sub
@@ -65,14 +70,14 @@ Public Module PublicFunctions
         ''' indice per poter settare la tipologia del bordo del form.
         ''' FormBorderStyle(myForm, FormBorderStyle.FixedSingle)
         ''' </summary>
-        Public Shared Sub formBorderStyle(form As Form, borderStyle As FormBorderStyle)
+        Public Shared Sub FormBorderStyle(form As Form, borderStyle As FormBorderStyle)
             form.FormBorderStyle = borderStyle
         End Sub
         ''' <summary>
         ''' indice per poter l'immagine di sfondo e il relativo layout.
         ''' FormBrgImage(myForm, "path/to/image.jpg", ImageLayout.Stretch)
         ''' </summary>
-        Public Shared Sub formBrgImage(form As Form, imagePath As String, layout As ImageLayout)
+        Public Shared Sub FormBrgImage(form As Form, imagePath As String, Optional ByVal layout As ImageLayout = Nothing)
             Dim image As Image = Image.FromFile(imagePath)
             form.BackgroundImage = image
             form.BackgroundImageLayout = layout
